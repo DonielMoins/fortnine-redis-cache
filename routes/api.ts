b19482ae82ee65edacbaf2f57b933682d0ac9a38
DonielMoins <fakhri.hamm@gmail.com>
2021-11-19 23:46:06 -0500

    little updates

--- routes/api.ts
+++ routes/api.ts
@@ -2,19 +2,14 @@ import express from "express";
 var router = express.Router()
 
 // middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
+router.use(function timeLog(_req: express.Request, _res: express.Response, next) {
     console.log('API: Time: ', Date.now())
     next()
 })
 // define the home page route
-router.get('/', function (req: express.Request, res: express.Response) {
-    if (req.params["force"]) {
-        redis_data = await redis
-    }
-})
-// define the about route
-router.get('/about', function (req, res) {
-    res.send('About birds')
+router.options('/', (req: express.Request, res: express.Response) => {
+    res.send("404")
 })
 
-module.exports = router
+
+export { router };
